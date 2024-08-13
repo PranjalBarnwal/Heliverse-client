af24da130dcc1a304f58ee1cd07239831841d639
Pranjal Barnwal <happybarnwal007@gmail.com>
2024-08-13 16:32:50 +0530

    final push

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 
-export const headerEP="https://heliverse-assignment-server-0sut.onrender.com/api/user"
+export const headerEP="https://heliverse-assignment-server-sdjq.onrender.com/api/user"
 // export const headerEP="http://localhost:3000/api/user"
